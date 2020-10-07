aaf49f7f95d904c9c2e1edc3b8b10ef263b8fd25
Jason Ozulumba <cjozulumba@gmail.com>
2020-10-06 23:45:54 -0400

    Added all add commands

--- app.js
+++ app.js
@@ -109,3 +109,118 @@ function employeeChoices() {
       }
     });
 }
+
+function addItem(item) {}
+
+async function addItem(item) {
+  if (err) throw err;
+  if (item === department) {
+    let { add } = await inquirer.prompt([
+      {
+        name: "add",
+        type: "input",
+        message: "What is the name of the department you want to add?",
+      },
+    ]);
+    connection.query(
+      `INSERT INTO ${item} SET ?;`,
+      {
+        name: add,
+      },
+      function (err, res) {
+        if (err) throw err;
+        console.log(res.affectedRows + " department inserted!\n");
+      }
+    );
+  } else if (item === role) {
+    let { add, money } = await inquirer.prompt([
+      {
+        name: "add",
+        type: "input",
+        message: "What is the name of the role you want to add?",
+      },
+      {
+        name: "money",
+        type: "input",
+        message: "How much should this role get paid?",
+      },
+    ]);
+    const chooseDepartment = connection.query(
+      "SELECT * FROM departments",
+      function (err, results) {
+        if (err) throw err;
+        
+        const { department } = await inquirer.prompt({
+            name: "department",
+            type: "list",
+            message: "Which department does this role belong to?",
+            choice: [results.name]
+        })
+        let data = results.forEach(options => {
+            if (options.name == department) {
+                return options.id;
+            };
+        });
+
+        return data;
+      }
+    );
+    connection.query(
+      `INSERT INTO ${item} SET ?;`,
+      {
+        title: add,
+        salary: money,
+        department_id: chooseDepartment
+      },
+      function (err, res) {
+        if (err) throw err;
+        console.log(res.affectedRows + " role inserted!\n");
+      }
+    );
+  } else if (item === employee) {
+    let { first, last } = await inquirer.prompt([
+        {
+          name: "first",
+          type: "input",
+          message: "What is the first name of this employee?",
+        },
+        {
+          name: "last",
+          type: "input",
+          message: "What is the last name of this employee?",
+        },
+      ]);
+      const chooseRole = connection.query(
+        "SELECT * FROM role",
+        function (err, results) {
+          if (err) throw err;
+          
+          const { role } = await inquirer.prompt({
+              name: "role",
+              type: "list",
+              message: "Which role is this employee gonna take?",
+              choice: [results.name]
+          })
+          let data = results.forEach(options => {
+              if (options.name == role) {
+                  return options.id;
+              };
+          });
+  
+          return data;
+        }
+      );
+    connection.query(
+      `INSERT INTO ${item} SET ?;`,
+      {
+        first_name: first,
+        last_name: last,
+        role_id: chooseRole
+      },
+      function (err, res) {
+        if (err) throw err;
+        console.log(res.affectedRows + " inserted!\n");
+      }
+    );
+  }
+}
